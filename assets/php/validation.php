8cd1caf44cb22f5891afe910e2da8549e9fd42f6
fredgaloppin <76943745+fredgaloppin@users.noreply.github.com>
2021-03-24 16:53:53 +0100

    Add files via upload

--- assets/php/validation.php
+++ assets/php/validation.php
@@ -1,80 +1,80 @@
 <?php
-// define variables and set to empty values
-$firstnameErr = $lastnameErr = $emailErr = $genderErr = $countryErr = "";
-$firstName = $lastName = $email = $gender = $messageInput = $country = $subjectInput = "";
+    // define variables and set to empty values
+    $firstnameErr = $lastnameErr = $emailErr = $genderErr = $countryErr = "";
+    $firstName = $lastName = $email = $gender = $messageInput = $country = $subjectInput = "";
 
-$nameMissingErr = "Name is required";
-$nameErr = "Only letters and white space allowed";
+    $nameMissingErr = "Name is required";
+    $nameErr = "Only letters and white space allowed";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["firstName"])) {
-    $firstnameErr = $nameMissingErr;
-  } else {
-    $firstName = test_input($_POST["firstName"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-      $firstnameErr = $nameErr;
-    }
-  }
-  
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["lastName"])) {
-      $lastnameErr = $nameMissingErr;
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["firstName"])) {
+        $firstnameErr = $nameMissingErr;
     } else {
-      $lastName = test_input($_POST["lastName"]);
-      // check if name only contains letters and whitespace
-      if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-        $lastnameErr = $nameErr;
-      }
+        $firstName = test_input($_POST["firstName"]);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+        $firstnameErr = $nameErr;
+        }
     }
-
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format";
-    }
-  }
     
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["lastName"])) {
+        $lastnameErr = $nameMissingErr;
+        } else {
+        $lastName = test_input($_POST["lastName"]);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+            $lastnameErr = $nameErr;
+        }
+        }
 
-  if (empty($_POST["messageInput"])) {
-    $messageInput = "";
-  } else {
-    $messageInput = test_input($_POST["messageInput"]);
-  }
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+    } else {
+        $email = test_input($_POST["email"]);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Invalid email format";
+        }
+    }
+        
 
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
-}
+    if (empty($_POST["messageInput"])) {
+        $messageInput = "";
+    } else {
+        $messageInput = test_input($_POST["messageInput"]);
+    }
 
-$country = isset($_POST['choiceCountry']) ? $_POST['choiceCountry'] : false;
-if ($country) {
-   echo htmlentities($_POST['choiceCountry'], ENT_QUOTES, "UTF-8");
-} else {
-    $countryErr = "Pick a country is required";
-  exit; 
-}
+    if (empty($_POST["gender"])) {
+        $genderErr = "Gender is required";
+    } else {
+        $gender = test_input($_POST["gender"]);
+    }
+    }
 
-$subjectInput = ($_POST["subjectInput"]);
+    $country = isset($_POST['choiceCountry']) ? $_POST['choiceCountry'] : false;
+    if ($country) {
+    echo htmlentities($_POST['choiceCountry'], ENT_QUOTES, "UTF-8");
+    } else {
+        $countryErr = "Pick a country is required";
+    exit; 
+    }
 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
+    $subjectInput = ($_POST["subjectInput"]);
 
-echo $firstName; echo <br>;
-echo $lastName; echo <br>;
-echo $gender; echo <br>;
-echo $email; echo <br>;
-echo $country; echo <br>;
-echo $subjectInput; echo <br>;
-echo $messageInput; 
+    function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+    }
+
+    echo $firstName; echo '<br>';
+    echo $lastName; echo '<br>';
+    echo $gender; echo '<br>';
+    echo $email; echo '<br>';
+    echo $country; echo '<br>';
+    echo $subjectInput; echo '<br>';
+    echo $messageInput; 
 
 ?>
